5c3481d0a93fa71e2611970bef5290e07de46897
duskan <kdg9037@gmail.com>
2018-09-07 04:23:32 +0900

    #4 리스트 뷰 생성, Model 일부분 수정

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from blog.views import IndexView
+from blog.views import IndexView, PostListView
 
 urlpatterns = [
     # ex: /polls/
     path('', IndexView.as_view(), name='index'),
+    path('post/', PostListView.as_view(), name='post'),
     # ex: /polls/5/
 ]
