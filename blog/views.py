63621c7d42b72f5ca761e97e3a69625449b35861
Duskan.Kwon <duskan.kwon@sendbird.com>
2020-07-21 17:21:09 +0900

    D

--- blog/views.py
+++ blog/views.py
@@ -40,7 +40,7 @@ def get_webhook(request):
     print(f"resquest ({type(request.data)})>>>")
     print("JWT result >>", jwt.decode(token, SECRET_KEY, ALGORITHM))
     decoded = jwt.decode(token, SECRET_KEY, ALGORITHM)
-    print("\t", hashlib.sha256(json.dumps(request.data)))
+    print("\t", hashlib.sha256(json.dumps(request.data).encode('utf-8')))
     print("\t", decoded["payload_hash"])
     print(request.data)
     print("----------------------------------------------------------")
